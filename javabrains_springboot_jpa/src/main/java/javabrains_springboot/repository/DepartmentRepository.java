01f7d0b803e162b87a4997c6980810a4460be237
Deadman <Deadman@Deadman>
2019-07-28 01:49:44 +0530

    Test Commit 2

--- javabrains_springboot_jpa/src/main/java/javabrains_springboot/repository/DepartmentRepository.java
+++ javabrains_springboot_jpa/src/main/java/javabrains_springboot/repository/DepartmentRepository.java
@@ -5,5 +5,7 @@ import org.springframework.data.repository.CrudRepository;
 import javabrains_springboot.pojo.Department;
 
 public interface DepartmentRepository extends CrudRepository<Department, String>{
+	
+	//
 
 }
